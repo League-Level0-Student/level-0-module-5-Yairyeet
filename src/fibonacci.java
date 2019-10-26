dc7e745c85d7c4542abbfcdd74f3004d0c24c69e
League Student <league@iMac-15.attlocal.net>
2019-10-25 18:27:19 -0700

    :sweat_smile:

---
+++ src/fibonacci.java
@@ -0,0 +1,28 @@
+
+public class fibonacci {
+public static void main(String[] args) {
+	for (int i = 0; i <12; i++) {
+		for (int j = 0; j < i; j++) {
+		
+	
+	
+	int seclast=j;
+	int last=i;
+	int yes=seclast+last;
+	System.out.println(yes);
+	}}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
