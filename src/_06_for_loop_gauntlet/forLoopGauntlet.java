ed8365ff9e44ecb27441aee1329249e0eba0686e
League Student <league@iMac-15.attlocal.net>
2019-11-22 18:26:17 -0800

    :smoking:

---
+++ src/_06_for_loop_gauntlet/forLoopGauntlet.java
@@ -0,0 +1,44 @@
+package _06_for_loop_gauntlet;
+
+public class forLoopGauntlet {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 101; i++) {
+		System.out.println(i);
+	}
+	for (int y = 100; y>0; y--) {
+		System.out.println(y);
+	}
+	for (int e = 2; e < 100; e++) {
+		System.out.println(e);
+	}
+	for (int u = 1; u < 99; u++) {
+		
+	}
+	for (int r = 1; r < 500; r++) {
+		System.out.println(r);
+		if(r%2==r) {
+			System.out.println("even");
+		}else {
+			System.out.println("odd");
+		}
+	}
+	for (int g = 0; g < 777; g++) {
+		
+	if(g%7==7) {
+		System.out.println(g);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
