8d6fc6bdc60faa813f9491e502a17c3931864428
League Student <league@iMac-15.attlocal.net>
2019-12-27 18:24:56 -0800

    :+1:

---
+++ src/MyJavaProgram.java
@@ -0,0 +1,10 @@
+
+
+class MyJavaProgram{
+	public static void main(String[] args){
+		String h = "horse";
+		for(int i = 5; i >= 0; i--){
+			System.out.println(h);
+		}
+	}
+}
--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -11,15 +11,17 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+JOptionPane.showInputDialog("how many hour did u spent coding\n this week");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+String code=JOptionPane.showInputDialog("if it's less then 2 then \nstop watching YouTube and \nwrite code insted and if\n u r codeing more then that\n u r  a Code Ninja\n if u did more then 5 hours say yes");
+	
+// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
 
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
