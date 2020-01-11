db62e4741f5ba75c99e0b4a6f60a6aae2f71e3a3
League Student <league@iMac-27.attlocal.net>
2020-01-10 18:27:34 -0800

    :+1:

---
+++ src/_11_whats_the_output/t6.java
@@ -0,0 +1,15 @@
+package _11_whats_the_output;
+
+public class t6 { public static void main(String[] args){
+	int x = 7;
+
+	if(x == 0){
+	  x = 10;
+	}else if(x < 5){
+	  x = 11;
+	}else{
+	  x = 12;
+	}
+
+
+}}
